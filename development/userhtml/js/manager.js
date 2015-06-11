952e964221606a63a7cdc7e8323700387493b305
Ryun <ryunhe@gmail.com>
2015-06-11 12:40:03 +0800

    177号段不能注册

--- development/userhtml/js/manager.js
+++ development/userhtml/js/manager.js
@@ -306,7 +306,7 @@ window.sysmanager = {
                     this.dom.userpanel_chepai.blur();
                     if('' == phone){
                         sysmanager.alert('手机号不能为空!');
-                    }else if(!(/^1[3|4|5|8][0-9]\d{8}$/.test(phone))){
+                    }else if(!(/^1[3|4|5|7|8][0-9]\d{8}$/.test(phone))){
 				            		sysmanager.alert('请输入正确的手机号!');
 				            }else{
                         sysmanager.login(phone,chepai,function(){
@@ -324,7 +324,7 @@ window.sysmanager = {
                     this.dom.userpanel_chepai.blur();
                     if('' == phone){
                         sysmanager.alert('手机号不能为空!');
-                    }if(!(/^1[3|4|5|8][0-9]\d{8}$/.test(phone))){
+                    }if(!(/^1[3|4|5|7|8][0-9]\d{8}$/.test(phone))){
 				            		sysmanager.alert('请输入正确的手机号!');
 				            }else{
                         sysmanager.login(phone,chepai,function(){
