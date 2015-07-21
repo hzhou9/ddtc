5dee30f171bc34d6e548dbde93971ac546ff805f
joe <hzhou9@gmail.com>
2015-07-21 17:47:28 +0800

    remove manual tixian/jifen

--- development/secenhtml/v.php
+++ development/secenhtml/v.php
@@ -5,6 +5,10 @@
     
     //todo:根据版本来设置p，实现内容动态更新
     $p = '';
+    if($view == 'tixianop' || $view == 'jifenop2'){
+        if($type == 'html')
+        $p = '?20150721';
+    }
 
     $url = $view.'/'.$view.'.'.$type.$p;
     
